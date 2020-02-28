68ea11a87c0e0a94afe5ee7a8f943b48be3be706
vlad <vladyslav.kostiuk@clurgo.com>
2020-02-28 20:09:25 +0100

    init commit

---
+++ src/main/java/com/clurgo/nanorent/NanorentApplication.java
@@ -0,0 +1,13 @@
+package com.clurgo.nanorent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanorentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanorentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clurgo/nanorent/NanorentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clurgo.nanorent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanorentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
