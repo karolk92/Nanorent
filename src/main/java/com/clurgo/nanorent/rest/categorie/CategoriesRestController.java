3b1f5ab1d6cde9edaf58035fd880b67ed4463a89
vlad <vladyslav.kostiuk@clurgo.com>
2020-02-28 21:29:53 +0100

    rest layer

---
+++ src/main/java/com/clurgo/nanorent/rest/categorie/CategoriesRestController.java
@@ -0,0 +1,7 @@
+package com.clurgo.nanorent.rest.categorie;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoriesRestController {
+}
---
+++ src/main/java/com/clurgo/nanorent/rest/resource/ResourcesRestController.java
@@ -0,0 +1,7 @@
+package com.clurgo.nanorent.rest.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourcesRestController {
+}
