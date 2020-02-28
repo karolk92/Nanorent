d8330449811d57bdba8182bd87225a4322f92c00
Karol Kwit <karol.kwit@clurgo.com>
2020-02-28 23:18:34 +0100

    sql entities created, postgres built in docker

--- src/main/java/com/clurgo/nanorent/com/clurgo/nanorent/entity/User.java
+++ src/main/java/com/clurgo/nanorent/com/clurgo/nanorent/entity/User.java
@@ -1,11 +1,17 @@
 package com.clurgo.nanorent.com.clurgo.nanorent.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
 @Entity
 public class User {
 
